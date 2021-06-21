26633a92773bf2408f14eea95e7327ee99aeb249
Lukas <lukas.ancikevicius@gmail.com>
2021-06-21 11:02:57 +0300

    new file structre

---
+++ config/config sample.js
@@ -0,0 +1,8 @@
+const mongoDbString = '<db string goes here>';
+
+// <password> - pakeisti i slaptazodi
+// myFirstDatabase - pakeisti i db pavadinima
+
+module.exports = {
+  mongoDbString,
+};
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const mongoDbString =
+  'mongodb+srv://lukasAdmin:lukoilas@frankfurtclusteraws.xj0nq.mongodb.net/reactDB?retryWrites=true&w=majority';
+
+module.exports = {
+  mongoDbString,
+};
